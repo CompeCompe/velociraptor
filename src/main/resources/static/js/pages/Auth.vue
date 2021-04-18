7cc089f9ad763596857d92eb7071bdfcabd5cf3d
Maxim Eremenko <mieremenko@processtech.ru>
2021-04-19 00:28:36 +0300

    decor auth page

--- src/main/resources/static/js/pages/Auth.vue
+++ src/main/resources/static/js/pages/Auth.vue
@@ -1,10 +1,34 @@
 <template>
-    <v-container>
-        Необходимо авторизоваться через
-        <a href="/login">Google</a>
-    </v-container>
-</template>
+<v-content>
+  <v-card
+    class="mx-auto"
+    max-width="400"
+    elevation="11"
+  >
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      src="https://img5.goodfon.ru/wallpaper/nbig/a/b5/art-minimalizm-dinozavr-velotsiraptor-velociraptor-synth-ret.jpg"
+    >
+    </v-img>
 
+    <v-card-text class="text--primary">
+      <div>Авторизуйтесь через доступный сервис</div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn
+        color="purple"
+        class="white--text"
+        text
+        href="/login"
+      >
+        Google
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</v-content>
+</template>
 <script>
     export default {
         name: "Auth",
